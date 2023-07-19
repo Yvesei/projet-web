26fba7b81857723142a123cb593d597678742043
Yvesei <musheck999@gmail.com>
2023-07-19 13:13:21 +0100

    Update look of categories

--- client/src/components/Articles.vue
+++ client/src/components/Articles.vue
@@ -3,9 +3,12 @@
 <div class="min-h-screen flex justify-center items-center py-20">
   <div class="container mx-auto p-12 rounded-xl">
     <h1 class="text-4xl font-bold text-center mb-8">Latest Posts</h1>
-    <div class="flex justify-center flex-wrap ">
-      <button class="uppercase m-1 rounded gray-dark px-5 py-3 text-base mb-3 font-medium text-white transition duration-200 hover:bg-gray-600 active:bg-blue-700 " v-for="categorie in categories" v-bind:key="categorie.id" >{{categorie.category}} <span class="bg-blue-700 p-1 rounded"> {{categorie.articleCount}} </span></button>
-   </div>
+    <div class="cat flex overflow-scroll ">
+
+<button class="uppercase m-1 rounded px-5 py-3 text-base mb-3 font-medium text-black transition duration-200 
+hover:bg-[#e1e1e1] active:bg-blue-700 " v-for="categorie in categories" v-bind:key="categorie.id" >{{categorie.category}} </button>
+
+</div>
     <div class="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0">
       <div v-for="article in articles" v-bind:key="article.id">
         <Single :article=article  />
