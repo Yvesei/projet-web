5e8e56527e9685e0b34014461efe5925581e7ae9
Yvesei <musheck999@gmail.com>
2023-07-19 13:14:07 +0100

    minimalist look

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -2,16 +2,12 @@
     <div class="bg-white relative lg:py-20">
   <div class="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto max-w-7xl
       xl:px-5 lg:flex-row">
-    <div class="flex flex-col items-center w-full pt-5 pr-10 pb-10 pl-10 lg:pt-10 lg:flex-row">
-      <div class="w-full bg-cover relative max-w-md lg:max-w-2xl lg:w-7/12">
-        <div class="flex flex-col items-center justify-center w-full h-full relative lg:pr-10">
-          <img src="https://res.cloudinary.com/macxenon/image/upload/v1631570592/Run_-_Health_qcghbu.png" class="btn-"/>
-        </div>
-      </div>
+    <div class="flex flex-col items-center w-full pt-5 pr-10 pb-10 pl-10 lg:pt-10 ">
+     
       <div class="w-full mt-20 mr-0 mb-0 ml-0 relative z-10 max-w-2xl lg:mt-0 lg:w-5/12">
         <div class="flex flex-col items-start justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl
             relative z-10">
-          <p class="w-full text-4xl font-medium text-center leading-snug font-serif">Login To Your Account</p>
+          <p class="w-full text-4xl font-medium text-center leading-snug ">Login To Your Account</p>
           <form @submit.prevent class="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
             
             <div class="relative">
@@ -28,7 +24,7 @@
                   border-gray-300 rounded-md"/>
             </div>
 
-            <a class="w-full text-sm font-medium leading-snug font-serif "><router-link to="/signup">Create an account ?</router-link></a>
+            <a class="w-full text-sm font-medium leading-snug  "><router-link to="/signup">Create an account ?</router-link></a>
 
             <div class="relative">
               <a @click="handleSubmit()" class="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-indigo-500
--- client/src/components/SignUp.vue
+++ client/src/components/SignUp.vue
@@ -2,16 +2,12 @@
     <div class="bg-white relative lg:py-20">
   <div class="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto max-w-7xl
       xl:px-5 lg:flex-row">
-    <div class="flex flex-col items-center w-full pt-5 pr-10 pb-10 pl-10 lg:pt-10 lg:flex-row">
-      <div class="w-full bg-cover relative max-w-md lg:max-w-2xl lg:w-7/12">
-        <div class="flex flex-col items-center justify-center w-full h-full relative lg:pr-10">
-          <img src="https://res.cloudinary.com/macxenon/image/upload/v1631570592/Run_-_Health_qcghbu.png" class="btn-"/>
-        </div>
-      </div>
+    <div class="flex flex-col items-center w-full pt-5 pr-10 pb-10 pl-10 lg:pt-10 ">
+      
       <div class="w-full mt-20 mr-0 mb-0 ml-0 relative z-10 max-w-2xl lg:mt-0 lg:w-5/12">
         <div class="flex flex-col items-start justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl
             relative z-10">
-          <p class="w-full text-4xl font-medium text-center leading-snug font-serif">Sign up for an account</p>
+          <p class="w-full text-4xl font-medium text-center leading-snug ">Sign up for an account</p>
           <form @submit.prevent class="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
             <div class="relative">
               <p class="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
